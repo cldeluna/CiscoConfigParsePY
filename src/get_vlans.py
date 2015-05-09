fbe2883742c719830e3c591e7190bb8b9ebfd0b9
Claudia <cldeluna@yahoo.com>
2015-05-09 10:29:24 -0700

    Check for Vlans and IPs on Interfaces

--- src/get_vlans.py
+++ src/get_vlans.py
@@ -16,3 +16,13 @@ for v in vl:
     print v
 print ("Number of items in list: %s" % lvl)
 print ("Vlans from config file: %s" % str(sys.argv[1]))
+
+raw_input("Press Return to continue to IP Section...")
+
+ipint = config.find_objects_w_child(parentspec=r"^interface",childspec=r"ip address")
+lipint = len(ipint)
+for i in ipint:
+    print i
+
+print ("Number of items in list: %s" % lipint)
+print ("IPs from config file: %s" % str(sys.argv[1]))
