28cbf4e0a25525dbd8bbdde9712f858262091179
Claudia <cldeluna@yahoo.com>
2015-05-09 10:30:47 -0700

    Check for Vlans and IPs on Interfaces

---
+++ src/get_ipints.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# get_vlans.py
+
+__author__ = 'Claudia'
+
+import sys
+
+#raw_input("Press Return to continue...")
+
+from ciscoconfparse import CiscoConfParse
+
+config = CiscoConfParse(str(sys.argv[1]),factory=True)
+vl = config.find_all_children("^vlan")
+lvl=len(vl)
+for v in vl:
+    print v
+print ("Number of items in list: %s" % lvl)
+print ("Vlans from config file: %s" % str(sys.argv[1]))
+
+raw_input("Press Return to continue to Router/SVI Section...")
+
+int = config.find_objects_w_child(parentspec=r"^interface",childspec=r"ip address")
+lint = len(int)
+for i in int:
+    print i
+
+print ("Number of items in list: %s" % lint)
+print ("Routed Interfaces from config file: %s" % str(sys.argv[1]))
+
+
+raw_input("Press Return to continue to IP Section...")
+
+ipint = config.find_interface_objects("^interface")
+lipint = len(ipint)
+for i in ipint:
+    print i
+
+print ("Number of items in list: %s" % lipint)
+print ("IPs from config file: %s" % str(sys.argv[1]))
