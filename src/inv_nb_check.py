4993304cdf4a8d3cd36884c2b1f9a93ead22474e
Claudia <cldeluna@yahoo.com>
2015-06-21 14:53:36 -0700

    First Working Version - Pints all by Default

---
+++ src/inv_nb_check.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python -tt
+# inv_nb_check.py
+# Claudia
+# PyCharm
+__author__ = "Claudia de Luna (claudia@indigowire.net)"
+__version__ = ": 1.0 $"
+__date__ = "6/21/15  12:17 PM"
+__copyright__ = "Copyright (c) 2015 Claudia de Luna"
+__license__ = "Python"
+
+import sys
+import re
+import os
+import xlrd
+
+
+#def var(var1):
+
+
+# Provided main() calls the above functions
+def main():
+    # Take path argument and list all text files
+    """
+    Description
+    :return:
+    """
+
+    siteid=sys.argv[1]
+    #siteid=1389
+
+    inv=sys.argv[2]
+    #inv="/Users/Claudia/Box Sync/Network Transformation/Inventory/DiData-inventory-2015-06-15.xlsx"
+
+    invsh="Chassis & Module"
+
+    nbdir=sys.argv[3]
+    #nbdir="/Users/Claudia/Box Sync/Network Transformation/Sites/North America - Year One/1389-Ontario-CA-Solar/show_commands_detail_06.15.15"
+
+    siterows=0
+    not_only="No"
+
+    nbfiles=os.listdir(nbdir)
+    #print nbfiles
+    #for file in nbfiles:
+        #print file
+
+    wb = xlrd.open_workbook(inv)
+    ws = wb.sheet_by_name(invsh)
+
+    print "DiData Inventory Workbook in use:, %s"%(inv)
+    print "DiData Inventory Worksheet in use:, %s"%(ws.name)
+    print "Total number of rows in the Inventory Worksheet:, %d"%(ws.nrows)
+    print "Total number of columns in the Inventory Worksheet:, %d"%(ws.ncols)
+    print "Netbrain (NB) Output file directory:, %s"%(nbdir)
+    print "Site ID to search:, %s"%(siteid)
+    print ("-")*80
+    print "Name, IP Address, Slot, Model, Filename, NB File Found"
+
+    for row_index in range(ws.nrows):
+        if ws.cell_value(row_index,6) == int(siteid):
+            sw=(ws.cell_value(row_index,1))
+            sw=sw.lower()
+            swfile=sw+".txt"
+            #print ws.cell_value(row_index,1)
+            #print ws.cell_type(row_index,6)
+            #print ws.cell_value(row_index,6)
+            siterows=siterows+1
+            #print sw
+            #print swfile
+            if swfile in nbfiles:
+                print "%s,%s,%s,%s,swfile,NB File Found"%(ws.cell_value(row_index,1),ws.cell_value(row_index,2),ws.cell_value(row_index,3),ws.cell_value(row_index,4))
+            else:
+                print "%s,%s,%s,%s,swfile,NB File NOT Found"%(ws.cell_value(row_index,1),ws.cell_value(row_index,2),ws.cell_value(row_index,3),ws.cell_value(row_index,4))
+    print ("-")*80
+    print str(siterows)
+
+
+
+# Standard call to the main() function.
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print '\nUsage: inv_nb_check.py.py <siteid> <full path to inventory file> <full path to NB show command output directory>\nExample: python inv_nb_check.py "1389" "/Users/Claudia/Box Sync/Network Transformation/Inventory/DiData-inventory-2015-06-15.xlsx" "/Users/Claudia/Box Sync/Network Transformation/Sites/North America - Year One/1389-Ontario-CA-Solar/show_commands_detail_06.15.15"\nNote: Processing can take up to a minute!\n\n'
+        sys.exit()
+    else:
+        main()
+
